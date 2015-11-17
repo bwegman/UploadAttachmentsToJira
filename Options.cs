134f52557cc793c7e2bb121cf231a014c2bb6ec5
Benjamin Wegman <wegman@shader.nl>
2015-11-17 18:06:46 +0100

    Typo

--- Options.cs
+++ Options.cs
@@ -17,7 +17,7 @@ namespace UploadAttachmentsToJira
         [Option('r', "project", Required = true, HelpText = "Project Key")]
         public string ProjectKey { get; set; }
 
-        [Option('i', "inputpath", Required = true, HelpText = "Path where to attachments are stored. Attachment filenames should be formatted '{ISSUE-ID} - AttachmentName.Ext' where ISSUE-ID is just the number of the Jira item to use")]
+        [Option('i', "inputpath", Required = true, HelpText = "Path where attachments are stored. Attachment filenames should be formatted '{ISSUE-ID} - AttachmentName.Ext' where ISSUE-ID is just the number of the JIRA item to use")]
         public string InputPath { get; set; }
 
         [Option('o', "overwrite", Required = false, HelpText = "Overwrites existing attachments")]
